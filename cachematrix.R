9df7f7458848b701ff4fba8adf17957ef8f61798
Andhra-1771553 <andhrakakumani@gmail.com>
2019-06-10 18:54:34 +0100

    Commiting the completed cachematrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## (makeCacheMatrix, cacheSolve) is a pair of functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
+## Following is a function for a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i       <- NULL
+        set     <- function(y){
+                x  <<- y
+                i  <<- NULL
+        }
+        get     <- function() x
+        setinv  <- function(isolve) i <<- isolve 
+        getinv  <- function() i 
+        list(set = set, get = get, 
+             setinv = setinv, 
+             getinv = getinv)
+        
 }
 
 
-## Write a short comment describing this function
+## The following function "cacheSolve" computes the inverse of the special "matrix" returned by "makeCacheMatrix"
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve      <- function(x, ...) {
+        
+        i       <- x$getinv()
+        if(!is.null(i)){
+                message("getting cached data")
+                return (i)
+        }
+        data    <- x$get()
+        i       <- solve(data)
+        x$setinv(i)
+        i
 }
+
+## Checking the pair of functions
+## Its important to keep the size of matrix to square, otherwise "Error in solve.default(data) : 'a' (4 x 3) must be square" will pop up!
+
+s               <- matrix(rpois(25,2),5,5)
+special         <- makeCacheMatrix(s)
+cacheSolve(special)
+
+## Results: Program worked like a gem.!
+
